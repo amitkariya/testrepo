f434230ab0c6cc10614b4d61b15bb4a887741bad
amitkariya <amit.kariya@gmail.com>
2019-03-07 22:21:24 +0530

    test

--- 2webpack.mix.js
+++ 2webpack.mix.js
@@ -12,52 +12,21 @@ const mix = require('laravel-mix');
  */
 
 mix.styles([
+    'resources/public/css/separate/pages/login.min.css',
+    'resources/public/css/lib/font-awesome/font-awesome.min.css',
+    'resources/public/css/lib/bootstrap/bootstrap.min.css',
     'resources/public/css/main.css'
-], 'public/css/main.css');
-
-mix.styles([
-    'resources/public/css/app.css'
-], 'public/css/app.css');
-
-mix.styles([
-    'resources/public/css/lib/bootstrap/bootstrap.min.css'
-], 'public/css/lib/bootstrap/bootstrap.min.css');
-
-mix.styles([
-    'resources/public/css/lib/font-awesome/font-awesome.min.css'
-], 'public/css/lib/font-awesome/font-awesome.min.css');
-
-mix.styles([
-    'resources/public/css/separate/pages/login.min.css'
-], 'public/css/separate/pages/login.min.css');
+], 'public/css/login.css');
 
 mix.scripts([
+    'resources/public/js/lib/jquery/jquery-3.2.1.min.js',
+    'resources/public/js/lib/popper/popper.min.js',
+    'resources/public/js/lib/tether/tether.min.js',
+    'resources/public/js/lib/bootstrap/bootstrap.min.js',
+    'resources/public/js/plugins.js',
+    'resources/public/js/lib/match-height/jquery.matchHeight.min.js',
     'resources/public/js/app.js'
-], 'public/js/app.js');
-
-mix.scripts([
-    'resources/public/js/plugins.js'
-], 'public/js/plugins.js');
-
-mix.scripts([
-    'resources/public/js/lib/bootstrap/bootstrap.min.js'
-], 'public/js/lib/bootstrap/bootstrap.min.js');
-
-mix.scripts([
-    'resources/public/js/lib/jquery/jquery-3.2.1.min.js'
-], 'public/js/lib/jquery/jquery-3.2.1.min.js');
-
-mix.scripts([
-    'resources/public/js/lib/match-height/jquery.matchHeight.min.js'
-], 'public/js/lib/match-height/jquery.matchHeight.min.js');
-
-mix.scripts([
-    'resources/public/js/lib/popper/popper.min.js'
-], 'public/js/lib/popper/popper.min.js');
-
-mix.scripts([
-    'resources/public/js/lib/tether/tether.min.js'
-], 'public/js/lib/tether/tether.min.js');
+], 'public/js/login.js');
 
 mix.copy('resources/public/img/company/pgsl_logo-min.jpg','public/img/company/pgsl_logo-min.jpg');
 mix.copy('resources/public/fonts/Proxima_Nova_Regular.ttf','public/fonts/Proxima_Nova_Regular.ttf');
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,13 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" href="{{ URL::asset('css/login.css') }}">
+    <!--<link rel="stylesheet" href="{{ URL::asset('css/login.css') }}">-->
+
+    <link rel="stylesheet" href="{{ URL::asset('css/separate/pages/login.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/lib/font-awesome/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/lib/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+
 </head>
 <body>
 
@@ -56,7 +62,13 @@
     </div><!--.page-center-->
 
 
-<script src="{{ URL::asset('js/login.js') }}"></script>
+<!--<script src="{{ URL::asset('js/login.js') }}"></script>-->
+<script src="{{ URL::asset('js/lib/jquery/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ URL::asset('js/lib/popper/popper.min.js') }}"></script>
+<script src="{{ URL::asset('js/lib/tether/tether.min.js') }}"></script>
+<script src="{{ URL::asset('js/lib/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('js/plugins.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/lib/match-height/jquery.matchHeight.min.js') }}"></script>
     <script>
         $(function() {
             $('.page-center').matchHeight({
@@ -73,5 +85,6 @@
             });
         });
     </script>
+    <script src="{{ URL::asset('js/app.js') }}"></script>
 </body>
 </html>
